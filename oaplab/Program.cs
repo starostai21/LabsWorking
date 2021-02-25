7c2f49cfe6589dc9b9474dcc8e0c4e9e938f01cf
MaksimMih <vip.peta.1998@mail.ru>
2021-02-25 13:48:20 +0300

    лаба

--- oaplab/Program.cs
+++ oaplab/Program.cs
@@ -14,41 +14,58 @@ namespace oapLabaSmirnov
         // точка входа в программу
         static void Main(string[] args)
         {/*
-            while (true)
+            var MassivDate = new List<DateTime>() { 
+                new DateTime(2021, 1, 15), 
+                new DateTime(2021, 2, 20),
+                new DateTime(2011, 5, 2),
+                new DateTime(2015, 5, 1),
+                new DateTime(2013, 5, 3),
+                new DateTime(2020, 1, 17)};
+            var result = new Dictionary<int, int>();
+            foreach(DateTime TekushayaData in MassivDate)
             {
-                Console.Write("Input Nomer: ");
-                string InputNomer = Console.ReadLine();
-                if (InputNomer.Length == 0)
-                    break;
-
-
-                char[] GosNomerSimb = { 'A', 'B', 'C', 'E', 'H', 'K', 'M', 'O', 'P', 'T', 'X', 'Y' };
-                if (InputNomer.Length == 6)
+                if (result.ContainsKey(TekushayaData.Month))
                 {
-                    var ValidNumber = true;
-                    for (int i = 0; i < 6 & ValidNumber; i++)
-                    {
-                        if (i == 1 | i == 2 | i == 3)
-                            ValidNumber &= Char.IsDigit(InputNomer[i]);
-                        else
-
-                            ValidNumber &= (InputNomer[i].ToString().IndexOfAny(GosNomerSimb) >= 0);
-                    }
-                    Console.Write(ValidNumber ? "номер правильный\n" : "номер не правильный\n");
+                    result[TekushayaData.Month] = result[TekushayaData.Month] + 1;
                 }
                 else
+                    result[TekushayaData.Month] = 1;
+            }
+            var PopularMonth = -1;
+            var MaxCount = 0;
+            foreach (KeyValuePair<int,int> keyValue in result)
+            {
+                if(keyValue.Value > MaxCount)
                 {
-                    Console.Write("пошли нафиг\n");
-                    
+                    MaxCount = keyValue.Value;
+                    PopularMonth = keyValue.Key;
                 }
-            }*/
-            Console.Write("Введите последовательность: ");
+            }
+            Console.WriteLine($"Самый популярный месяц {PopularMonth}");
+            */
+            Console.WriteLine("Input posledovatelnost: ");
             string Posledovatelnost = Console.ReadLine();
-            char[] Strelka1 = { '>', '<', '-' };
-            if (Posledovatelnost == Strelka1[0] &  )
-            Console.Write("\nPress ENTER to continue...");
+            var StrelkaCoint = 0;
+            var strelka1 = ">>-->";
+            var strelka2 = "<--<<";
+            int poz = 0;
+            while((poz = Posledovatelnost.IndexOf(strelka1, poz))>=0)
+            {
+                StrelkaCoint++;
+                poz++;
+            }
+            poz = 0;
+            while ((poz = Posledovatelnost.IndexOf(strelka2, poz)) >= 0)
+            {
+                StrelkaCoint++;
+                poz++;
+            }
+            Console.WriteLine($"Количество стрелок: {StrelkaCoint}");
+
+
+            Console.Write("Press ENTER to continue...");
             Console.ReadLine();
-         }
+        }
     }
  }
  
