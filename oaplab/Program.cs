c17c1eb0df665f6249d09fcbb67ba4ab445e0c4e
MaksimMih <vip.peta.1998@mail.ru>
2021-02-25 15:11:57 +0300

    лаба новая

--- oaplab/Program.cs
+++ oaplab/Program.cs
@@ -43,6 +43,7 @@ namespace oapLabaSmirnov
             }
             Console.WriteLine($"Самый популярный месяц {PopularMonth}");
             */
+            /*
             Console.WriteLine("Input posledovatelnost: ");
             string Posledovatelnost = Console.ReadLine();
             var StrelkaCoint = 0;
@@ -61,7 +62,43 @@ namespace oapLabaSmirnov
                 poz++;
             }
             Console.WriteLine($"Количество стрелок: {StrelkaCoint}");
+            */
+            /*
 
+            DateTime date1 = new DateTime(2021, 02, 21);
+            DateTime date2 = new DateTime(2026, 02, 25);
+            Console.WriteLine(Math.Abs(date1.Subtract(date2).TotalDays));
+            */
+            /*
+            Console.WriteLine("Введите год: ");
+            DateTime date1 = new DateTime(Convert.ToInt32(Console.ReadLine()), 1, 1);
+            date1 = date1.AddDays(255);
+            var DateString = date1.ToString("dd MMMM");
+            Console.WriteLine($"день программиста отмечается  {DateString}");
+            */
+            /*
+            Console.WriteLine("Введите день: ");
+            DateTime date1 = new DateTime(2021, 1, 1);
+            date1 = date1.AddDays(Convert.ToInt32(Console.ReadLine())-1);
+            var DateString = date1.ToString("dddd");
+            var voskresenie = date1.DayOfWeek == DayOfWeek.Sunday ? 7 : (int)date1.DayOfWeek;
+            Console.WriteLine($"день недели {DateString}, и номер дня недели {voskresenie}");
+            */
+            DateTime date1 = new DateTime(2021, 06, 21);
+            Console.WriteLine("Введите дату в формате дд.мм.гггг: ");
+            string date2 = Console.ReadLine();
+            string[] StringArray = date2.Split('.');
+            var Date3 = new DateTime(Convert.ToInt32(StringArray[2]), Convert.ToInt32(StringArray[1]), Convert.ToInt32(StringArray[0]));
+            var Raznitca = date1.Subtract(Date3);
+            if (Raznitca.TotalDays == 0)
+            {
+                Console.WriteLine("Сегодня экзамен");
+            }
+            else if (Raznitca.TotalDays > 0)
+                Console.WriteLine($"Экзамен будет через {Raznitca.TotalDays} дней");
+            else 
+                Console.WriteLine($"Экзамен был {Math.Abs(Raznitca.TotalDays)} дней назад");
+            
 
             Console.Write("Press ENTER to continue...");
             Console.ReadLine();
--- readme.md
+++ readme.md
@@ -1,5 +1,27 @@
-Лаболаторная работа 
-Выполнила **Пермякова Света"
+<table style="width: 100%;">
+  <tr>
+    <td style="text-align: center; border: none;">
+    Министерство образования и науки РФ<br><br>
+Государственное бюджетное профессиональное образовательное учреждение Республики Марий Эл<br><br>
+Йошкар-Олинский технологический колледж
+</td>
+  </tr>
+  <tr>
+    <td style="text-align: center; border: none; height: 15em;"><h2>Методические рекомендации по подготовке к демо-экзамену.</h2></td>
+  </tr>
+  <tr>
+    <td style="text-align: right; border: none; height: 20em;">
+      Разработал:<br/>
+      Смирнов Евгений Алексеевич
+    </td>
+  </tr>
+  <tr>
+    <td style="text-align: center; border: none; height: 5em;">
+    г.Йошкар-Ола, 2021</td>
+  </tr>
+</table>
+
+<div style="page-break-after: always;"></div>
 
 
 
