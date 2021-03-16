a0e7da22a11a065a0b9f63d3994631742c08f5e0
MaksimMih <vip.peta.1998@mail.ru>
2021-03-16 10:27:22 +0300

    лаба новая

--- oaplab/Program.cs
+++ oaplab/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 // namespace такой же как и название проекта
 namespace oapLabaSmirnov
@@ -14,10 +15,64 @@ namespace oapLabaSmirnov
          // точка входа в программу
          static void Main(string[] args)
          {
-  
+            //Exercise();
+            //Exercise1();
+            Exercise2();
+         }
+        
+        static void Exercise()
+        {
+            Console.WriteLine("Введите строку для поиска времени: ");
+            var InpString = Console.ReadLine();
+            Regex RegexExpretion = new Regex(@"([0][1-9]|[1][0-9]|[2][0-3]):([0-5][0-9])");
+            var Result = RegexExpretion.Match(InpString);
+            
+            if (RegexExpretion.IsMatch(InpString))
+            {
+                Console.WriteLine(Result.Value);
+            }            
+            else
+            {
+                Console.WriteLine("неккоректное время");
+            }
+                           
+            
+            Console.ReadKey();
+        }
+        static void Exercise1()
+        {
+            Console.WriteLine("Введите строку для поиска html цвета: ");
+            var InpString = Console.ReadLine();
+            Regex RegexExpretion = new Regex(@"#([0-9]|[A-F]){6}");
+            var Result = RegexExpretion.Match(InpString);
 
+            if (RegexExpretion.IsMatch(InpString))
+            {
+                Console.WriteLine(Result.Value);
+            }
+            else
+            {
+                Console.WriteLine("неккоректный цвет");
+            }
+            Console.ReadKey();
+        }
+        static void Exercise2()
+        {
+            Console.WriteLine("Введите строку для поиска арифметических операций: ");
+            var InpString = Console.ReadLine();
+            Regex RegexExpretion = new Regex(@"([-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)([+]|[-]|[*][\/]?)([-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)");
+            var Result = RegexExpretion.Match(InpString);
+
+            if (RegexExpretion.IsMatch(InpString))
+            {
+                Console.WriteLine(Result.Value);
+            }
+            else
+            {
+                Console.WriteLine("неккоректная строка");
+            }
+            Console.ReadKey();
         }
      }
  }
- 
-    
+
